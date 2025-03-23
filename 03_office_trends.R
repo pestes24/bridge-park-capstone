4d82ec4478fdadd94e6156630ec137f246e17462
hoh2006 <hoh2006@nyu.edu>
2025-03-23 18:28:22 -0400

    Everything until GGPLOT

--- 03_office_trends.R
+++ 03_office_trends.R
@@ -9,8 +9,6 @@ library(ggplot2)
 library(stringr)
 library(lubridate)
 
-R.version
-
 install.packages("urbnthemes")
 
 library(urbnthemes)
@@ -109,3 +107,43 @@ dc_OFFICE_trends_sales <- dcOFFICESalePriceInflation %>%
 
 # write.csv(dc_OFFICE_trends_rent, file.path(peter_export, "dc_OFFICE_trends_rent.csv"))
 # write.csv(dc_OFFICE_trends_sales, file.path(peter_export, "dc_OFFICE_trends_sales.csv"))
+
+# JOINING across geos -----------------------------------------------------------
+office_trends_rent <- ana_OFFICE_trends_rent %>%
+  bind_rows(eotr_OFFICE_trends_rent) %>% 
+  bind_rows(dc_OFFICE_trends_rent) %>% 
+  mutate(across(starts_with("asking"), as.double),
+         year = as.numeric(str_sub(period, 1, 4)),  # Extract year
+         quarter = as.numeric(str_sub(period, 7, 7)),  # Extract quarter number
+         period_date = make_date(year, (quarter - 1) * 3 + 1, 1)  # Convert to first day of the quarter
+  ) %>% 
+  pivot_wider(names_from = geo_inflation, # COMMENT for some reason, R could not find the object geo here; had to make it geo_inflation in order to do the pivot wider
+  values_from = c("market_asking_rent_inflation","market_asking_rent_noinflation","asking_rent_inflation","asking_rent_noinflation")) %>% 
+  filter(period %in% c("2024 Q4", "2024 Q3", "2024 Q2", "2024 Q1", "2023 Q4", "2023 Q3", "2023 Q2", "2023 Q1", "2022 Q4", "2022 Q3", "2022 Q2", "2022 Q1", "2021 Q4", "2021 Q3", "2021 Q2", "2021 Q1", "2020 Q4", "2020 Q3", "2020 Q2", "2020 Q1", "2019 Q4", "2019 Q3", "2019 Q2", "2019 Q1", "2018 Q4", "2018 Q3", "2018 Q2", "2018 Q1", "2017 Q4", "2017 Q3", "2017 Q2", "2017 Q1", "2016 Q4", "2016 Q3", "2016 Q2", "2016 Q1", "2015 Q4", "2015 Q3", "2015 Q2", "2015 Q1")) 
+  
+#filtering for 2015-2024
+#lots of missing data for office
+
+office_trends_sales <- ana_OFFICE_trends_sales %>% 
+  bind_rows(eotr_OFFICE_trends_sales) %>% 
+  mutate(across(starts_with("sale"), as.double)) %>% 
+  bind_rows(dc_OFFICE_trends_sales) %>% 
+  mutate(year = as.numeric(str_sub(period, 1, 4)),  # Extract year
+         quarter = as.numeric(str_sub(period, 7, 7)),  # Extract quarter number
+         period_date = make_date(year, (quarter - 1) * 3 + 1, 1)  # Convert to first day of the quarter
+  ) %>% 
+  pivot_wider(names_from = geo_inflation, # COMMENT for some reason, R could not find the object geo here; had to make it geo_inflation in order to do the pivot wider
+  values_from = c("sale_price_per_sf_inflation", "sale_price_per_sf_noinflation")) %>% 
+  filter(period %in% c("2024 Q4", "2024 Q3", "2024 Q2", "2024 Q1", "2023 Q4", "2023 Q3", "2023 Q2", "2023 Q1", "2022 Q4", "2022 Q3", "2022 Q2", "2022 Q1", "2021 Q4", "2021 Q3", "2021 Q2", "2021 Q1", "2020 Q4", "2020 Q3", "2020 Q2", "2020 Q1", "2019 Q4", "2019 Q3", "2019 Q2", "2019 Q1", "2018 Q4", "2018 Q3", "2018 Q2", "2018 Q1", "2017 Q4", "2017 Q3", "2017 Q2", "2017 Q1", "2016 Q4", "2016 Q3", "2016 Q2", "2016 Q1", "2015 Q4", "2015 Q3", "2015 Q2", "2015 Q1")) 
+#filtering for 2015-2024
+
+# Figures using inflation data ------------------------------------------------
+
+rent_inflation_plot <- office_trends_rent %>% 
+mutate(geo_inflation = factor(geo_inflation, levels = c("Anacostia", "eotr", "dc"))) %>%
+  ggplot(aes(
+    x = period,
+    y = market_asking_rent_inflation_Anacostia, # COMMENT because R couldn't find Geo earlier, I think it didn't make a new column with all of the values in it and it 
+  
+
+  
